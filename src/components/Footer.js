c4031d8de24dbbee7b9d0b0b7632b68171e305e5
Gokcenomer <b.ylmz55@hotmail.com>
2023-02-15 17:39:08 +0300

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Copyright &#169 2023 Gokcenomer;</p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
