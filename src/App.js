3c333fa324f2b4359e24803b2327a2c804a5804d
Gokcenomer <b.ylmz55@hotmail.com>
2023-02-19 18:45:24 +0300

    refactored functional component

--- src/App.js
+++ src/App.js
@@ -4,173 +4,148 @@ import GeneralInformation from "./components/GeneralInformation";
 import PracticalExperience from "./components/PracticalExperience";
 import EducationalExperience from "./components/EducationalExperience";
 // import uniqid from "uniqid";
-import React, { Component } from "react";
+
 import "./styles/style.css";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      email: "",
-      phoneNumber: "",
-      schoolName: "",
-      titleOfStudy: "",
-      dateOfStudy: "",
-      companyName: "",
-      mainTask: "",
-      positionTitle: "",
-      dateFrom: "",
-      dateTo: "",
-      isSubmitted: false,
-    };
-  }
-  handleInputChange = (e) => {
-    // console.log(e.target.name);
+import React, { useState } from "react";
 
-    this.setState({
-      [e.target.name]: e.target.value,
+const App = () => {
+  const [formValues, setFormValues] = useState({
+    name: "",
+    email: "",
+    phoneNumber: "",
+    schoolName: "",
+    titleOfStudy: "",
+    dateOfStudy: "",
+    companyName: "",
+    mainTask: "",
+    positionTitle: "",
+    dateFrom: "",
+    dateTo: "",
+  });
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
     });
   };
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.setState({
-      isSubmitted: true,
-    });
+  const handleSubmit = () => {
+    setIsSubmitted(true);
   };
-  handleEdit = (e) => {
-    e.preventDefault();
-    this.setState({
-      isSubmitted: false,
-    });
+  const handleEdit = () => {
+    setIsSubmitted(false);
   };
+  return isSubmitted ? (
+    <div>
+      <GeneralInformation
+        name={formValues.name}
+        email={formValues.email}
+        phoneNumber={formValues.phoneNumber}
+      />
+      <PracticalExperience
+        schoolName={formValues.schoolName}
+        titleOfStudy={formValues.titleOfStudy}
+        dateOfStudy={formValues.dateOfStudy}
+      />
+      <EducationalExperience
+        companyName={formValues.companyName}
+        mainTask={formValues.mainTask}
+        positionTitle={formValues.positionTitle}
+        dateFrom={formValues.dateFrom}
+        dateTo={formValues.dateTo}
+      />
+      <button onClick={handleEdit}>Edit</button>
+    </div>
+  ) : (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="name"
+          type="text"
+          placeholder="Name"
+          onChange={handleInputChange}
+          value={formValues.name}
+        />
 
-  render() {
-    const {
-      name,
-      email,
-      phoneNumber,
-      schoolName,
-      titleOfStudy,
-      dateOfStudy,
-      companyName,
-      mainTask,
-      positionTitle,
-      dateFrom,
-      dateTo,
-    } = this.state;
-    if (this.state.isSubmitted) {
-      return (
-        <div>
-          <GeneralInformation
-            name={name}
-            email={email}
-            phoneNumber={phoneNumber}
-          />
-          <PracticalExperience
-            schoolName={schoolName}
-            titleOfStudy={titleOfStudy}
-            dateOfStudy={dateOfStudy}
-          />
-          <EducationalExperience
-            companyName={companyName}
-            mainTask={mainTask}
-            positionTitle={positionTitle}
-            dateFrom={dateFrom}
-            dateTo={dateTo}
-          />
-          <button onClick={this.handleEdit}>Edit</button>
-        </div>
-      );
-    }
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          onChange={handleInputChange}
+          value={formValues.email}
+        />
 
-    return (
-      <div className="App">
-        <form onSubmit={this.handleSubmit}>
-          <input
-            name="name"
-            type="text"
-            placeholder="Name"
-            onChange={this.handleInputChange}
-            value={name}
-          />
+        <input
+          name="phoneNumber"
+          type="text"
+          placeholder="PhoneNumber"
+          onChange={handleInputChange}
+          value={formValues.phoneNumber}
+        />
+        <input
+          name="schoolName"
+          type="text"
+          placeholder="School Name"
+          onChange={handleInputChange}
+          value={formValues.schoolName}
+        />
+        <input
+          name="titleOfStudy"
+          type="text"
+          placeholder="Title Of Study"
+          onChange={handleInputChange}
+          value={formValues.titleOfStudy}
+        />
+        <input
+          name="dateOfStudy"
+          type="text"
+          placeholder="Date Of Study"
+          onChange={handleInputChange}
+          value={formValues.dateOfStudy}
+        />
+        <input
+          name="companyName"
+          type="text"
+          placeholder="Company Name"
+          onChange={handleInputChange}
+          value={formValues.companyName}
+        />
+        <input
+          name="mainTask"
+          type="text"
+          placeholder="Main Task"
+          onChange={handleInputChange}
+          value={formValues.mainTask}
+        />
+        <input
+          name="positionTitle"
+          type="text"
+          placeholder="Position Title"
+          onChange={handleInputChange}
+          value={formValues.positionTitle}
+        />
+        <input
+          name="dateFrom"
+          type="text"
+          placeholder="Date From"
+          onChange={handleInputChange}
+          value={formValues.dateFrom}
+        />
+        <input
+          name="dateTo"
+          type="text"
+          placeholder="Date To"
+          onChange={handleInputChange}
+          value={formValues.dateTo}
+        />
 
-          <input
-            name="email"
-            type="text"
-            placeholder="Email"
-            onChange={this.handleInputChange}
-            value={email}
-          />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
 
-          <input
-            name="phoneNumber"
-            type="text"
-            placeholder="PhoneNumber"
-            onChange={this.handleInputChange}
-            value={phoneNumber}
-          />
-          <input
-            name="schoolName"
-            type="text"
-            placeholder="School Name"
-            onChange={this.handleInputChange}
-            value={schoolName}
-          />
-          <input
-            name="titleOfStudy"
-            type="text"
-            placeholder="Title Of Study"
-            onChange={this.handleInputChange}
-            value={titleOfStudy}
-          />
-          <input
-            name="dateOfStudy"
-            type="text"
-            placeholder="Date Of Study"
-            onChange={this.handleInputChange}
-            value={dateOfStudy}
-          />
-          <input
-            name="companyName"
-            type="text"
-            placeholder="Company Name"
-            onChange={this.handleInputChange}
-            value={companyName}
-          />
-          <input
-            name="mainTask"
-            type="text"
-            placeholder="Main Task"
-            onChange={this.handleInputChange}
-            value={mainTask}
-          />
-          <input
-            name="positionTitle"
-            type="text"
-            placeholder="Position Title"
-            onChange={this.handleInputChange}
-            value={positionTitle}
-          />
-          <input
-            name="dateFrom"
-            type="text"
-            placeholder="Date From"
-            onChange={this.handleInputChange}
-            value={dateFrom}
-          />
-          <input
-            name="dateTo"
-            type="text"
-            placeholder="Date To"
-            onChange={this.handleInputChange}
-            value={dateTo}
-          />
-
-          <button>Submit</button>
-        </form>
-      </div>
-    );
-  }
-}
 export default App;
